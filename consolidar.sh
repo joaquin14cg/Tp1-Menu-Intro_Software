40dbd0d42981654c5d5823a5f42e6ef31971d354
Hugo-LL1 <hleloutre@fi.uba.ar>
2026-09-02 16:39:10 -0300

    Retiro las variables del menu

--- consolidar.sh
+++ consolidar.sh
@@ -6,9 +6,9 @@ do
     for archivo in entrada/*.txt
     do 
         FECHA_HORA=$(date "+%d/%m/%Y %T")
-        cat "$archivo" >> $CARPETA_SALIDA/$FILENAME.txt
-        mv "$archivo" $CARPETA_PROCESADO
-        echo "$FECHA_HORA - Procesado archivo $(basename "$archivo")" >> $ARCHIVO_LOG
+        cat "$archivo" >> salida/$FILENAME.txt
+        mv "$archivo" procesado/
+        echo "$FECHA_HORA - Procesado archivo $(basename "$archivo")" >> procesado/procesado.log
     done
 sleep 30    
 done                            
