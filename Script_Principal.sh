9ba0cffe2e8b4275b7eb999d9e130a3dc938403b
sofimachalekk <sofia.a.machalek@gmail.com>
2026-09-02 17:51:13 -0300

    Borro comentarios

--- Script_Principal.sh
+++ Script_Principal.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#test lo que hace es evaluar una codicion y devuelve 0 si paso con exito o 1 en caso contrario, al ponerle la flag -f
-#lo que evalua es si existe algo en la ubicacion que le pase a continuacion y si es un archivo
-
-# sort lo que hace es ordenar de menor a mayor, y con las flags le decimos que es lo que tiene que ordenar
-# con -k1,1 lo que le decimos es agarra la columna 1 hasta la columna 1, entonces solo ordena la 1
-# con la n que va seguida del segundo 1 en verdad es la flag -n y sirve para que interprete los valores como numeros y no como texto
-
-#head muestra los primeros elementos de una lista y con -n le podemos decir cuantos queremos que nos muestre
-
 if [[ -z $FILENAME ]]; then
     FILENAME="alumnos"
 fi
@@ -27,7 +18,6 @@ if [[ $1 = "-d" ]]; then
         pkill -f $CONSOLIDAR
         rm -r -f $DIRECTORIO_EPNRO1
 fi
-
 
 opcion=0
 
