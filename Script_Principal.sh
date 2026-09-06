69dde2f1f336b972fcac5e90a4de59dce1ba7d32
sofimachalekk <sofia.a.machalek@gmail.com>
2026-09-06 18:57:59 -0300

    Emprolijo script y ordeno variables

--- Script_Principal.sh
+++ Script_Principal.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-if [[ -z $FILENAME ]]; then
-    FILENAME="alumnos"
-fi
-
-ALUMNOS="$HOME/EPNro1/salida/$FILENAME.txt"
 DIRECTORIO_EPNRO1="$HOME/EPNro1"
-CARPETA_ENTRADA="$DIRECTORIO_EPNRO1/entrada"
-CARPETA_SALIDA="$DIRECTORIO_EPNRO1/salida"
-CARPETA_PROCESADO="$DIRECTORIO_EPNRO1/procesado"
-ARCHIVO_FINAL="$CARPETA_SALIDA/$FILENAME.txt"
-ARCHIVO_LOG="$CARPETA_PROCESADO/procesado.log"
 CONSOLIDAR="$DIRECTORIO_EPNRO1/consolidar.sh"
+ARCHIVO_LOG="$DIRECTORIO_EPNRO1/procesado/procesado.log"
+ARCHIVO_FINAL="$DIRECTORIO_EPNRO1/salida/$FILENAME.txt"
 
 if [[ $1 = "-d" ]]; then
         echo "ejecutando limpieza"
@@ -30,65 +22,67 @@ echo "5- Datos de Alumno"
 echo "6- Visualizar log"
 echo "7- Salir"
 read -p "Elija una opcion: " opcion
-case $opcion in
-        1)
-          echo "Creando entorno..."
-          mkdir -p $DIRECTORIO_EPNRO1/{entrada,salida,procesado}
-          if [[ -f consolidar.sh ]]; then
-                  cp consolidar.sh $DIRECTORIO_EPNRO1
-                  chmod +x $CONSOLIDAR
-          else
-                  echo "No se encuentra el archivo consolidar.sh"
-          fi
-          echo "Entorno creado"
-          ;;
-        2)
-          if [[ -f $CONSOLIDAR ]]; then
-                    echo "Corriendo proceso..."
-                    $CONSOLIDAR &
-          else
-             echo -n "No se ha encontrado consolidar.sh"
-             echo "Debe seleccionar la Opcion 1 primero"
-          fi
-          ;;
-        3) 
-        if test -f "$ALUMNOS"; then
-        sort -k1,1n "$ALUMNOS"
-        else
-        echo "No se encuentra el archivo $FILENAME.txt"
-        fi
-        ;;
-        4) 
-        if test -f "$ALUMNOS"; then
-        sort -k5,5nr "$ALUMNOS" | head -n 10
-        else
-        echo "No se encuentra el archivo $FILENAME.txt"
-        fi
-        ;;
-        5) 
-        echo "Ingrese un número de padrón (solo números):"
-        read numeropadron
 
-        buscar=$(grep -w "$numeropadron" $ALUMNOS)
+        case $opcion in
+                1)
+                echo "Creando entorno..."
+                mkdir -p $DIRECTORIO_EPNRO1/{entrada,salida,procesado}
+                if [[ -f consolidar.sh ]]; then
+                        cp consolidar.sh $DIRECTORIO_EPNRO1
+                        chmod +x $CONSOLIDAR
+                else
+                        echo "No se encuentra el archivo consolidar.sh"
+                fi
+                echo "Entorno creado"
+                ;;
+                2)
+                if [[ -f $CONSOLIDAR ]]; then
+                        echo "Corriendo proceso..."
+                        $CONSOLIDAR &
+                else
+                        echo -n "No se ha encontrado consolidar.sh"
+                        echo "Debe seleccionar la Opcion 1 primero"
+                fi
+                ;;
+                3) 
+                if test -f "$ARCHIVO_FINAL"; then
+                        sort -k1,1n "$ARCHIVO_FINAL"
+                else
+                        echo "No se encuentra el archivo $FILENAME.txt"
+                fi
+                ;;
+                4) 
+                if test -f "$ARCHIVO_FINAL"; then
+                        sort -k5,5nr "$ARCHIVO_FINAL" | head -n 10
+                else
+                        echo "No se encuentra el archivo $FILENAME.txt"
+                fi
+                ;;
+                5) 
+                echo "Ingrese un número de padrón (solo números):"
+                read numeropadron
 
-        until [[ "$numeropadron" =~ ^[0-9]+$ ]] && [[ -n "$buscar" ]]
-        do 
-        echo "Número de padrón no encontrado. Ingrese uno válido (solo números): "
-        read numeropadron
-        buscar=$(grep -w "$numeropadron" $ALUMNOS)
+                buscar=$(grep -w "$numeropadron" $ARCHIVO_FINAL)
 
-        done
-        echo "Datos del padrón:"
-        echo "$buscar"
+                until [[ "$numeropadron" =~ ^[0-9]+$ ]] && [[ -n "$buscar" ]]
+                do 
+                        echo "Número de padrón no encontrado. Ingrese uno válido (solo números): "
+                        read numeropadron
+                        buscar=$(grep -w "$numeropadron" $ARCHIVO_FINAL)
 
-        ;;
-        6)
-        cat $ARCHIVO_LOG
-        ;;
-        7)echo "Saliendo..."
-                exit 0
-          ;;
-        *)echo "Opcion invalida, intente nuevamente"
-          ;;
-esac
+                done
+                echo "Datos del padrón:"
+                echo "$buscar"
+                ;;
+                6)
+                cat $ARCHIVO_LOG
+                ;;
+                7)
+                echo "Saliendo..."
+                        exit 0
+                ;;
+                *)
+                echo "Opcion invalida, intente nuevamente"
+                ;;
+        esac
 done
